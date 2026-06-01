a82e989ade4e1c931a30d516cf061dedbae2195b
blackh-t <nache7383@uit.no>
2026-06-01 16:28:51 +0200

    feat: db --- get_uid()

--- api_gateway/src/db/mod.rs
+++ api_gateway/src/db/mod.rs
@@ -1,1 +1,11 @@
+use sqlx::PgPool;
+use uuid::Uuid;
+
 pub mod deposit;
+
+pub async fn get_uid(pool: &PgPool, wallet: &str) -> Result<Uuid, sqlx::Error> {
+    sqlx::query_scalar("SELECT uid FROM users WHERE LOWER(wallet_address) = LOWER($1)")
+        .bind(wallet)
+        .fetch_one(pool)
+        .await
+}
