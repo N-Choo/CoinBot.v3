63625de4ae580054df2814cafa96ff45f00daee6
blackh-t <nache7383@uit.no>
2026-06-06 18:06:17 +0200

    redesign: homepage - clean landing with inline stats, connect-aware CTA

--- react/src/pages/homePage.tsx
+++ react/src/pages/homePage.tsx
@@ -1,7 +1,5 @@
 import '../styles/homepage.css'
 import HeroSection from '../components/landing/HeroSection'
-import StatsSection from '../components/landing/StatsSection'
-import ReactorSection from '../components/landing/ReactorSection'
 import FeaturesSection from '../components/landing/FeaturesSection'
 import FooterSection from '../components/landing/FooterSection'
 
@@ -9,8 +7,6 @@ export default function Landing() {
   return (
     <div className="landing">
       <HeroSection />
-      <StatsSection />
-      <ReactorSection />
       <FeaturesSection />
       <FooterSection />
     </div>
